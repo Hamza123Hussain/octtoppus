ebb58c9815d0f326c9affa820d625012aa41a5a4
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-08 18:37:43 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import Body from '@/components/Homepage/Body'
 import Navbar from '@/components/Navbar/Navbar'
 
 export default function Home() {
-  return <Navbar />
+  return (
+    <div className=" flex flex-col bg-black">
+      <Navbar />
+      <Body />
+    </div>
+  )
 }
